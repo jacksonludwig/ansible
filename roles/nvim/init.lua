67572c2951ac97ab7f3fadb001e867fdac310d7f
Jackson Ludwig <jacksonludwig0@gmail.com>
2021-12-27 18:55:52 -0500

    make stylua executable

--- roles/nvim/init.lua
+++ roles/nvim/init.lua
@@ -2,6 +2,7 @@
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local packer_bootstrap = nil
+
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({
     "git",
