af09069d8d2fb0db28b4ddd1982ef1890d93fb7f
Jackson Ludwig <jacksonludwig0@gmail.com>
2022-01-01 15:28:38 -0500

    update nvim, kitty, flatpak remotes

--- roles/nvim/init.lua
+++ roles/nvim/init.lua
@@ -111,7 +111,7 @@ require("packer").startup(function(use)
     "nvim-telescope/telescope.nvim",
     requires = {
       "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-fzf-native.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
     },
     config = function()
       local telescope = require("telescope")
