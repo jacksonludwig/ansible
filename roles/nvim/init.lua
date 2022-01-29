e84b4931ab5a9d52764a9a025e94667ab4f5fbd1
Jackson Ludwig <jacksonludwig0@gmail.com>
2022-01-29 18:57:18 -0500

    update luasnip binds

--- roles/nvim/init.lua
+++ roles/nvim/init.lua
@@ -450,10 +450,10 @@ require("packer").startup(function(use)
     "L3MON4D3/LuaSnip",
     config = function()
       vim.cmd([[
-        imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-        inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
-        snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
-        snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+        imap <silent><expr> <C-j> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<C-j>' 
+        inoremap <silent> <C-k> <cmd>lua require'luasnip'.jump(-1)<Cr>
+        snoremap <silent> <C-j> <cmd>lua require('luasnip').jump(1)<Cr>
+        snoremap <silent> <C-k> <cmd>lua require('luasnip').jump(-1)<Cr>
       ]])
     end,
   })
