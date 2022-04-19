253212a2d2c2400f032214f4adfbeeaff2cbec76
Jackson Ludwig <jacksonludwig0@gmail.com>
2022-04-19 15:23:41 -0400

    update stylua link and hl grp name

--- roles/nvim/init.lua
+++ roles/nvim/init.lua
@@ -512,12 +512,12 @@ vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "*",
 })
 
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+local yank_highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
+  group = yank_highlight_group,
   pattern = "*",
 })
