612ff6d7f356480ba74f73b1b34208f7abfad952
Jackson Ludwig <jacksonludwig0@gmail.com>
2022-07-30 23:32:19 -0400

    use company & save corfu stuff for now

--- roles/emacs/init.el
+++ roles/emacs/init.el
@@ -39,10 +39,10 @@
 (menu-bar-mode -1)
 
 ;;; Completion
-(straight-use-package 'cape)
+;; (straight-use-package 'cape)
 (straight-use-package 'consult)
-(straight-use-package 'corfu-doc)
-(straight-use-package 'corfu)
+;; (straight-use-package 'corfu-doc)
+;; (straight-use-package 'corfu)
 (straight-use-package 'embark)
 (straight-use-package 'embark-consult)
 (straight-use-package 'marginalia)
@@ -79,39 +79,39 @@
   (add-hook 'embark-collect-mode-hook #'consult-preview-at-point-mode))
 
 ;; Corfu
-(customize-set-variable 'corfu-cycle t) ; Allows cycling through candidates
-(customize-set-variable 'corfu-auto t)  ; Enable auto completion
-(customize-set-variable 'corfu-auto-prefix 2) ; Complete with less prefix keys
-(customize-set-variable 'corfu-auto-delay 0.25) ; Small delay for completion
-(customize-set-variable 'corfu-echo-documentation 0.25) ; Echo docs for current completion option
-(customize-set-variable 'corfu-doc-auto nil)
+;; (customize-set-variable 'corfu-cycle t) ; Allows cycling through candidates
+;; (customize-set-variable 'corfu-auto t)  ; Enable auto completion
+;; (customize-set-variable 'corfu-auto-prefix 2) ; Complete with less prefix keys
+;; (customize-set-variable 'corfu-auto-delay 0.25) ; Small delay for completion
+;; (customize-set-variable 'corfu-echo-documentation 0.25) ; Echo docs for current completion option
+;; (customize-set-variable 'corfu-doc-auto nil)
 
-(global-corfu-mode 1)
+;; (global-corfu-mode 1)
 
-(add-hook 'corfu-mode-hook #'corfu-doc-mode)
-(define-key corfu-map (kbd "M-p") #'corfu-doc-scroll-down)
-(define-key corfu-map (kbd "M-n") #'corfu-doc-scroll-up)
-(define-key corfu-map (kbd "M-d") #'corfu-doc-toggle)
+;; (add-hook 'corfu-mode-hook #'corfu-doc-mode)
+;; (define-key corfu-map (kbd "M-p") #'corfu-doc-scroll-down)
+;; (define-key corfu-map (kbd "M-n") #'corfu-doc-scroll-up)
+;; (define-key corfu-map (kbd "M-d") #'corfu-doc-toggle)
 
 ;; Cape
-(require 'cape)
+;; (require 'cape)
 
 ;; Add useful defaults completion sources from cape
-(add-to-list 'completion-at-point-functions #'cape-file)
-(add-to-list 'completion-at-point-functions #'cape-dabbrev)
+;; (add-to-list 'completion-at-point-functions #'cape-file)
+;; (add-to-list 'completion-at-point-functions #'cape-dabbrev)
 
 ;; Silence the pcomplete capf, no errors or messages!
 ;; Important for corfu
-(advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
+;; (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
 
 ;; Ensure that pcomplete does not rite to the buffer
 ;; and behaves as a pure `completion-at-point-function'.
-(advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify)
-(add-hook 'eshell-mode-hook
-          (lambda () (setq-local corfu-quit-at-boundary t
-                            corfu-quit-no-match t
-                            corfu-auto nil)
-            (corfu-mode)))
+;; (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify)
+;; (add-hook 'eshell-mode-hook
+;;           (lambda () (setq-local corfu-quit-at-boundary t
+;;                             corfu-quit-no-match t
+;;                             corfu-auto nil)
+;;             (corfu-mode)))
 
 ;;; Evil
 (straight-use-package 'evil)
@@ -195,12 +195,18 @@
 (straight-use-package 'lsp-mode)
 
 ;;; Corfu/Cape completion options (remove if using company)
-(setq lsp-completion-provider :none)
-(defun corfu-lsp-setup ()
-  (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
-        '(orderless)))
-(add-hook 'lsp-completion-mode-hook #'corfu-lsp-setup)
-(advice-add #'lsp-completion-at-point :around #'cape-wrap-noninterruptible)
+;; (setq lsp-completion-provider :none)
+;; (defun corfu-lsp-setup ()
+;;   (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+;;         '(orderless)))
+;; (advice-add #'lsp-completion-at-point :around #'cape-wrap-noninterruptible)
+
+;;; Company
+(straight-use-package 'company)
+(require 'company)
+(straight-use-package 'company-box)
+(require 'company-box)
+(add-hook 'company-mode-hook 'company-box-mode)
 
 (setq lsp-use-plists t)
 (setq lsp-headerline-breadcrumb-enable nil)
@@ -223,6 +229,7 @@
 (require 'lsp-mode)
 (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
 (add-hook 'typescriptreact-mode-hook 'lsp)
+;; (add-hook 'lsp-completion-mode-hook #'corfu-lsp-setup)
 
 ;;; Magit
 (straight-use-package 'magit)
@@ -241,21 +248,26 @@
 ;;; Theme
 (straight-use-package 'doom-themes)
 (require 'doom-themes)
-(load-theme 'doom-xcode t)
+(load-theme 'modus-vivendi t)
 
 ;;; Bindings
 (straight-use-package 'general)
 (require 'general)
+;; (general-def
+;;  :keymaps 'completion-in-region-mode
+;;  :definer 'minor-mode
+;;  :states 'insert
+;;  :predicate 'corfu-mode
+;;  "C-n" 'corfu-next
+;;  "C-p" 'corfu-previous
+;;  "C-y" 'corfu-complete
+;;  "C-e" 'corfu-quit
+;;  "<return>" 'newline)
 (general-def
- :keymaps 'completion-in-region-mode
- :definer 'minor-mode
- :states 'insert
- :predicate 'corfu-mode
- "C-n" 'corfu-next
- "C-p" 'corfu-previous
- "C-y" 'corfu-complete
- "C-e" 'corfu-quit
- "<return>" 'newline)
+  :keymaps 'company-active-map
+  "C-y" 'company-complete-selection
+  "C-e" 'company-abort
+  "<return>" 'newline)
 (general-def
   :states 'normal
   "<escape>" 'evil-ex-nohighlight
