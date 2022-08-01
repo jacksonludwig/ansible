e40c5675c377759a4633e7fc49cd2840122aa61a
Jackson Ludwig <jacksonludwig0@gmail.com>
2022-08-01 17:04:25 -0400

    fix: add hook that was deleted accidentally

--- roles/emacs/init.el
+++ roles/emacs/init.el
@@ -118,8 +118,8 @@
 (straight-use-package 'tree-sitter-langs)
 (require 'tree-sitter)
 (require 'tree-sitter-langs)
-;; (global-tree-sitter-mode)
-;; (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+(global-tree-sitter-mode)
+(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
 ;;; JSON
 (straight-use-package 'json-mode)
